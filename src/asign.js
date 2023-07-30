8df088d2d316896bccc787e6fb38a70eac796928
William Ruales <ruales@hotmail.co.uk>
2023-07-30 10:57:42 +0100

    object + list functions. Style added

---
+++ src/asign.js
@@ -0,0 +1,3 @@
+export default function asign() {
+    console.log("pingping");
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 console.log("this works")
+import "./style.css";
 
+const form = document.getElementById('form');
+
+form.addEventListener('submit', callbackFunction);
+let list = []
+
+function callbackFunction(event) {
+    event.preventDefault();
+    const myFormData = new FormData(event.target);
+    console.log(myFormData);
+
+    list.push(myFormData)
+    console.log(list)
+}
 const NewTask = function (name, weight) {
     this.name = name
     this.weight = weight
