828ef4aac73215c70cb3ab78aa814a5432653e59
William Ruales <ruales@hotmail.co.uk>
2023-08-08 20:37:35 +0100

    blank

--- src/index.js
+++ src/index.js
@@ -2,30 +2,58 @@ console.log("this works")
 import "./style.css";
 
 const form = document.getElementById('form');
-const createNew = document.querySelector(".createNew");
-const modal = document.querySelector(".modal")
+const newTask = document.querySelector(".createNew");
+const modal = document.querySelector(".modal");
+const taskbox = document.querySelector(".tasks")
+const list = [];
 
-form.addEventListener('submit', callbackFunction);
-let list = []
-createNew.addEventListener("click", popUp)
+form.addEventListener('submit', submit);
+
+
+newTask.addEventListener("click", popUp);
 function popUp() {
     modal.style.display = "flex";
-
+    console.log("run")
 }
 
-function callbackFunction(event) {
+function submit(event) {
     event.preventDefault();
-    const myFormData = new FormData(event.target);
-    console.log(myFormData);
+addtoList()
+domManipulation()
 
-    list.push(myFormData)
-    console.log(list)
     modal.style.display = "none"
-}
-const NewTask = function (name, weight) {
-    this.name = name
-    this.weight = weight
+    form.reset()
+
+
 }
 
-let doit = new NewTask(1, 2)
-console.log(doit)
+let addtoList = function addtoList() {
+    let task = [];
+    for (let i = 0; i < form.elements.length; i++) {
+        const element = form.elements[i];
+        if (element.tagName === "INPUT" || element.tagName === "TEXTAREA") {
+            task.push(element.value);
+
+        }
+       
+    }
+ list.push(task)
+    console.log("list:", list);
+}
+
+
+ function domManipulation() {
+    for (let i of list) {
+        updatebox(i)
+        
+    }
+}
+
+function updatebox(x){
+console.log(x)
+for(let q of x){
+taskbox.textContent= q;
+}
+
+}
+console.log(list, "MO")
