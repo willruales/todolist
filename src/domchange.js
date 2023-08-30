27020d61e9092d0e14fc0abc968d9b58232520f2
William Ruales <ruales@hotmail.co.uk>
2023-08-30 18:03:34 +0100

    quick befor merge

--- src/domchange.js
+++ src/domchange.js
@@ -1,28 +1,16 @@
-import { tableElement } from "./templates.js/tasktable";
-import projects from './projects.js';
-import { templateElement } from "./templates.js/tableRow";
-
-const form = document.getElementById('form');
-const modal = document.querySelector(".modal");
+const form = document.querySelector('.form');
+const projectpopup = document.getElementById("project-popup")
 const projectList = document.querySelector(".projectList")
 const taskbox = document.querySelector(".tasks")
 
-export default function domchange(event, callback) {
-    event.preventDefault();
+export default function domchange(x) {
 
-    let projectName = projects.projectName;
     const projectItem = document.createElement("div");
-    let htmlString = `<li class="project">${projectName}<button class="delete-button">x</button></li> `;
+    let htmlString = `<li class="project" data-userid =${x}>${x}<button class="delete-button">x</button></li> `;
 
     projectItem.innerHTML = htmlString;
     projectList.appendChild(projectItem)
-    modal.style.display = "none"
-
-    projectList.addEventListener("click", function () {
-        //taskbox.innerHTML = tableElement;
-        taskbox.appendChild(tableElement);
-
-    })
+    projectpopup.style.display = "none"
     form.reset()
 
     return projectItem
